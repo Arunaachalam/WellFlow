d96073cb97529523fe25cae3d43856bc345f7df9
Arunaachalam Muralidharan <arunaachalam.m@gmail.com>
2021-09-19 11:33:36 +0200

    Add files via upload

---
+++ FunctionalModules.py
@@ -0,0 +1,94 @@
+import numpy as np
+
+
+def newton_step(p_newton_old, dt, n):
+    """
+    Performs a Newtons step
+
+    :param p_newton_old: Old pressure array
+    :param dt: Time Step
+    :param n: Grid Length
+    :return: Newton stepped Pressure
+    """
+    gradient = np.gradient(p_newton_old, 1.0/(n))
+
+    # Eliminate Zeros and Nans
+    new_divide = np.zeros(n+1)
+    for i in range(len(gradient)-1):
+        if gradient[i] !=0:
+            new_divide[i] = p_newton_old[i]/gradient[i]
+        else:
+            new_divide[i] = 0
+
+    p_newton_new = p_newton_old + dt * new_divide
+
+    return p_newton_new
+
+
+def calculate_pd(p_newton_new, p_newton_old, dt,
+                 delta_y, n, alpha_d, lambda_d):
+    """
+    Calculate next pd time step
+    :param p_newton_new: Pressure from Newton step
+    :param p_newton_old: Old pressure values
+    :param dt: Time step
+    :param delta_y: Space step
+    :param n: Total grid points
+    :param alpha_d: Alpha value
+    :param lambda_d: Lambda value
+    :return:
+    """
+    pd = p_newton_old
+
+    # Calculate Pd(N-1)
+    rhs = 0
+    dp01 = (p_newton_new[1] - p_newton_new[0]) / delta_y
+    if dp01 != 0:
+        term1 = (((-2 * p_newton_new[n - 1]) + p_newton_new[n - 2]) / (
+                delta_y ** 2)) * ((1 + lambda_d) ** 2) * (
+                        1.0 / (dp01 ** 2))
+        term2 = (n - 1) * delta_y * ((1 + lambda_d) ** 2) * (
+                (p_newton_new[n - 1] - (2 * p_newton_new[n - 1])) / (
+                delta_y ** 2)) * (1.0 / (dp01 ** 2))
+        term3 = alpha_d * (lambda_d ** 2) * (1 + (delta_y * (n - 1)))
+        rhs = term1 + term2 - term3
+        pd[n - 1] = p_newton_old[n - 1] + (dt * rhs)
+
+    # Calculate Pd -> 1 to N-2
+    for i in range(len(p_newton_new) - 2, 1, -1):#, 1, -1):
+        rhs = 0
+
+        # Old Method without equation Modification
+        # Leave Now, scheme is unstable
+
+        #term1 = (p_newton_new[i + 1] - (2 * p_newton_new[i]) + p_newton_new[
+        #    i - 1]) / (delta_y ** 2)
+        #term21 = alpha_d * p_newton_new[n - 1] * (i - 1)
+        #term22 = (((1 + lambda_d) ** 2) / lambda_d ** 2) * i * ((p_newton_new[
+        #                                                             n - 2] - (
+        #                                                                 2 *
+        #                                                                 p_newton_new[
+        #                                                                     n - 1])) / delta_y ** 2) * \
+        #         p_newton_new[n - 1]
+        #term2 = ((p_newton_new[i + 1] - p_newton_new[i]) / delta_y) * (
+        #        term21 - term22)
+
+        # New Method with Modifications
+
+        term1 = (pd[i + 1] - (2 * pd[i]) + pd[i - 1]) / (delta_y ** 2)
+        term21 = alpha_d * pd[n - 1] * (i - 1)
+        term22 = (((1 + lambda_d) ** 2) / lambda_d ** 2) * i * ((pd[n - 2] - (2 * pd[n - 1])) / delta_y ** 2) * pd[n - 1]
+        term2 = ((pd[i + 1] - pd[i]) / delta_y) * (term21 - term22)
+
+        rhs = ((1 + lambda_d) ** 2) * (term1 + term2)
+
+        pd[i] += (dt * rhs)
+
+        # Leave the next line for now. Some instabilities there
+        #pd[i] = p_newton_old[i] + (dt * rhs)
+
+
+    # Calculate Pd 0
+    pd[0] = pd[1] + ((1 + lambda_d) / lambda_d) * pd[n - 1]
+
+    return pd
---
+++ main.py
@@ -0,0 +1,57 @@
+from FunctionalModules import *
+import numpy as np
+import argparse
+from matplotlib import pyplot as plt
+import pandas as pd
+
+
+def simulate(args):
+    """
+    Simulate the scenario
+    :param args: Arguments in
+    :return: None
+    """
+    t = 0
+    time = args.time
+    step_size = args.step_size
+    alpha_d = args.alpha
+    lambda_d = args.lambda_d
+    n = 20
+    p_newton_new = np.zeros(n + 1)
+    p_newton_old = np.zeros(n + 1)
+    p_newton_old[0] = -10
+    p_newton_old[n] = 0
+    p_d = np.zeros(n + 1)
+    p_do = pd.DataFrame(columns=['t', 'p_d0'])
+
+    while t < time:
+        p_newton_new = newton_step(p_newton_old, step_size, n)
+        p_d = calculate_pd(p_newton_new, p_newton_old, step_size, 1.0 / n, n,
+                           alpha_d, lambda_d)
+        p_newton_old = p_d
+
+
+        t += step_size
+
+        # Add plot functions and store functions if necessary
+
+        # p_do = p_do.append({'t': t, 'p_d0': p_d[0]}, ignore_index=True)
+
+    #plt.plot(p_do.t, p_do.p_d0)
+    #plt.show()
+
+
+if __name__ == '__main__':
+    arg_parser = argparse.ArgumentParser(description=__doc__)
+    arg_parser.add_argument('--time', type=int, default=10000,
+                            help='Total time for simulation')
+    arg_parser.add_argument('--step_size', type=float, default=10,
+                            help='Time step Size')
+    arg_parser.add_argument('--alpha', type=float, default=0.008,
+                            help='Alpha_D value')
+    arg_parser.add_argument('--lambda_d', type=float, default=0.852,
+                            help='Lambda_D value')
+
+    arguments = arg_parser.parse_args()
+
+    simulate(arguments)
